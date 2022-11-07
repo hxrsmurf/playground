049edd571e076aaded0938050da1cc6476afa91e
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-07 18:49:50 -0500

    Import spotify weba pi node even though we're working on password

--- nextjs-password/app/nowplaying/page.tsx
+++ nextjs-password/app/nowplaying/page.tsx
@@ -1,25 +1,38 @@
 'use client'
-import useSWR from 'swr'
 import { useEffect, useState } from 'react'
+
+var SpotifyWebApi = require('spotify-web-api-node')
+var spotifyApi = new SpotifyWebApi()
 
 export default function page() {
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(false)
+  const [profile, setProfile]: any = useState()
 
   useEffect(() => {
     setLoading(true)
     const getData = async () => {
       const query = await fetch('http://localhost:3000/api/nowplaying')
       const data = await query.json()
-      console.log(data)
       setData(data.access_token)
-      setLoading(false)
     }
     getData()
+    setLoading(false)
   }, [])
+
+  useEffect(() => {
+    if (data) {
+      spotifyApi.setAccessToken(data)
+      spotifyApi.getMe().then((me: { body: any }) => {
+        console.log(me.body)
+        setProfile(me.body)
+      })
+    }
+  }, [data])
 
   if (isLoading) return <>Loading...</>
   if (!data) return <>Loading data...</>
+  if (!profile) return <>Loading profile...</>
 
   return (
     <div className='bg-[#f1f5f8] text-black mt-12'>
@@ -27,6 +40,7 @@ export default function page() {
         <div className='grid grid-cols-1 place-content-center space-y-8 text-center'>
           <div className='font-bold'>Now Playing</div>
           <div>Your access token is: {data}</div>
+          <div>Your name is probably: {profile.display_name} </div>
         </div>
       </div>
     </div>
