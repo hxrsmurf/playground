7e60b418cbeca3efa701b8d81f080b42757d6e45
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-07 22:13:16 -0500

    Add any type to LoginButton

--- nextjs-password/app/login/loginbutton.tsx
+++ nextjs-password/app/login/loginbutton.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function LoginButton({ spotify_url }) {
+export default function LoginButton({ spotify_url }: any) {
   return (
     <Link href={spotify_url}>
       <button className='rounded-md bg-white text-black w-40 h-40'>
