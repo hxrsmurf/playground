56ea15ccf872b57acfa063d1094b0392e89ef5e1
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-11 15:09:09 -0500

    Add addl. encryption functions

--- nextjs-password/utils/encryption.ts
+++ nextjs-password/utils/encryption.ts
@@ -19,3 +19,21 @@ export async function aes256String(string: string) {
   //const decrypted = CryptoJS.AES.decrypt(encrypted, process.env.CRYPTO_SECRET)
   return encrypted
 }
+
+export async function decryptServerSideMasterKey(string: string) {
+  const bytes = CryptoJS.AES.decrypt(string, process.env.CRYPTO_SECRET)
+  const originalText = bytes.toString(CryptoJS.enc.Utf8)
+  return originalText
+}
+
+export async function customAES256Key(string: string, key: string) {
+  const encrypted = CryptoJS.AES.encrypt(string, key)
+  return encrypted.toString()
+}
+
+export async function decryptCustomAES256Key(string: string, key: string) {
+  const decrypted_key = decryptServerSideMasterKey(key, process.env.CRYPTO_SECRET)
+  const encrypted = CryptoJS.AES.decrypt(string, decrypted_key)
+  const original = encrypted.toString(CryptoJS.enc.Utf8)
+  return original
+}
