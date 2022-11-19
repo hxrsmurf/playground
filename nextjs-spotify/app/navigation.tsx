08ca0fca8b67146c7140f2ca96ac649d385ab870
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-19 07:56:15 -0500

    Hide Navigation Items if Not Logged In

--- nextjs-spotify/app/navigation.tsx
+++ nextjs-spotify/app/navigation.tsx
@@ -42,16 +42,27 @@ export default function navigation() {
 
   return (
     <div className='bg-blue-500 min-w-full min-h-[75px] flex justify-center pt-4 text-3xl space-x-8'>
-      {links.map((link) => (
-        <div>
-          <Link href={link.url} className='hover:bg-yellow-400'>
-            {link.name}
-          </Link>
-        </div>
-      ))}
-      <div>
-        <Login cookies={cookies} />
-      </div>
+      {cookies.access_token ? (
+        <>
+          {links.map((link) => (
+            <div>
+              <Link href={link.url} className='hover:bg-yellow-400'>
+                {link.name}
+              </Link>
+            </div>
+          ))}
+
+          <div>
+            <Login cookies={cookies} />
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <Login cookies={cookies} />
+          </div>
+        </>
+      )}
     </div>
   )
 }
