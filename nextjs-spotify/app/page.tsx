97abe7136da931d7de2c11becf7a2689000cd10c
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-20 07:15:39 -0500

    Make Login server-side

--- nextjs-spotify/app/page.tsx
+++ nextjs-spotify/app/page.tsx
@@ -1,33 +1,9 @@
-'use client'
-
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-
-async function getSpotifyLink() {
-  const res = await fetch('/api/auth/generateSpotifyLink')
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
-}
+import Login from './login'
 
 export default function Home() {
-  const [spotify_link, setSpotifyLink]: any = useState()
-
-  useEffect(() => {
-    const generateLink = async () => {
-      const resp = await getSpotifyLink()
-      setSpotifyLink(resp)
-    }
-    generateLink()
-  }, [])
-
-  if (!spotify_link) return <></>
-
   return (
     <div className='flex justify-center color:white'>
-      <Link href={spotify_link.link}>Login to Spotify</Link>
+      <Login />
     </div>
   )
 }
