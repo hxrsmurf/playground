80079fc23f339f2a772fb4b8c2304c0357ec3cf5
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-21 21:17:45 -0500

    Update Home Page to be Lookup

--- nextjs-twilio/app/page.tsx
+++ nextjs-twilio/app/page.tsx
@@ -1,57 +1,45 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
+import { useRouter } from 'next/navigation'
+
+export default function page() {
+  const router = useRouter()
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const number_to_search: any = document.querySelector('#number')
+    const actual_number = number_to_search.value
+    const parsed_number = parseNumber(actual_number)
+    router.push('/lookup/' + actual_number)
+  }
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div className='flex justify-center mt-4'>
+      <div>
+        <div>Lookup a number - US Only</div>
+        <form className='grid mt-4' onSubmit={(e) => handleSubmit(e)}>
+          <input
+            id='number'
+            type='tel'
+            className='mt-4 py-2 pl-3'
+            pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+            defaultValue='555-555-5555'
+          />
+          <button className='mt-4 rounded-lg bg-blue-500 px-4 py-2'>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   )
+}
+
+function parseNumber(number: string) {
+  if (number.includes(' ')) {
+    return number.replaceAll(' ', '')
+  }
+  if (number.includes('-')) {
+    return number.replaceAll('-', '')
+  }
+  return number
 }
