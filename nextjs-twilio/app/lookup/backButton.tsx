2a64525cac517ff52c9f924a3771e4c1b6754e8b
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-21 21:22:29 -0500

    Add loading dialog

--- nextjs-twilio/app/lookup/backButton.tsx
+++ nextjs-twilio/app/lookup/backButton.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function backButton() {
   return (
     <div className='mt-4 bg-blue-500 rounded-lg p-3 text-center'>
-      <Link href='/lookup'>Go Back</Link>
+      <Link href='/'>Go Back</Link>
     </div>
   )
 }
