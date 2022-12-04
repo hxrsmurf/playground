6389338ebbd9bc41240fbd694f7b733e61ea43ce
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 16:02:39 -0500

    Add concurrent futures for redis_set_data

--- python-pictures/index.py
+++ python-pictures/index.py
@@ -1,35 +1,12 @@
-import os
 import json
-import io
-from PIL import Image
+import concurrent.futures
 
 from functions.db import redis_check_existing, redis_set_data
-from functions.exif import get_exif_data
+from functions.list_files import list_files
 
 directory = 'C:/Users/kvchm/Pictures/pictures/'
+all_files = list_files(directory)
 
-for root, subdirs, files in os.walk(directory):
-    for file in files:
-        if '.jpg'in file or '.png' in file or '.JPG' in file:
-            full_path = root + '/' + file
-            redis_data = redis_check_existing(full_path)
-
-            if redis_data:
-                try:
-                    redis_metadata = json.loads(redis_data['metadata'])
-                    redis_binary_file = io.BytesIO(redis_data['binary_file'])
-                    # Show Image
-                    # Image.open(redis_binary_file).show()
-                except Exception as e:
-                    print(e)
-            else:
-                add_binary_file = True
-
-                if add_binary_file:
-                    metadata = get_exif_data(full_path)
-                    binary_file = open(full_path, 'rb').read()
-                else:
-                    metadata = full_path
-                    binary_file = 'empty'
-
-                redis_set_data(full_path, metadata, binary_file)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for file in all_files:
+        executor.submit(redis_set_data(file, file, file))
