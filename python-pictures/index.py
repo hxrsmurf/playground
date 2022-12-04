6e7b15ec2b3f0008283b263f9c2967abf0c6272e
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 09:56:22 -0500

    Initial ExifToolHelper

--- python-pictures/index.py
+++ python-pictures/index.py
@@ -1,8 +1,18 @@
 import os
+import exiftool
 
 directory = 'C:/Users/kvchm/Pictures/pictures/Ollie'
 
 for root, subdirs, files in os.walk(directory):
     for file in files:
-        full_path = root + '/' + file
-        print(full_path)
+
+        if not '.ini' in file:
+            full_path = root + '/' + file
+            with exiftool.ExifToolHelper() as et:
+                metadata = et.get_metadata(full_path)
+
+            for data in metadata:
+                for key, value in data.items():
+                    print(key, value)
+
+            break
