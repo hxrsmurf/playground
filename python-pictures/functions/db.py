9a0b314a8418dfaec0019ecd73258482473f8121
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 11:09:38 -0500

    Decode Redis HSET

--- python-pictures/functions/db.py
+++ python-pictures/functions/db.py
@@ -4,19 +4,39 @@ import json
 def redis_client():
     return redis.Redis(
         host='localhost',
-        port='6379'
+        port='6379',
     )
 
 def redis_check_existing(full_path):
     print(f'Retrieving from Redis:', full_path)
-    return redis_client().get(full_path)
 
-def redis_set_data(full_path, metadata):
+    decoded_data = {}
+
+    redis_data = redis_client().hgetall(full_path)
+
+    for key, values in redis_data.items():
+        decoded_key = key.decode('utf-8')
+        if decoded_key == 'metadata':
+            decoded_data[decoded_key] = values.decode('utf-8')
+        else:
+            decoded_data[decoded_key] = values
+
+    return decoded_data
+
+def redis_set_data(full_path, metadata, binary_file):
     try:
         print(f'Adding to Redis:', full_path)
-        redis_client().set(
-            full_path, json.dumps(metadata)
+        redis_client().hset(
+            full_path, 'metadata', json.dumps(metadata)
         )
+
+        set_binary = True
+
+        if set_binary:
+
+            redis_client().hset(
+                full_path, 'binary_file', binary_file
+            )
         return True
     except:
         return False
--- python-pictures/index.py
+++ python-pictures/index.py
@@ -4,17 +4,22 @@ import json
 from functions.db import redis_client, redis_check_existing, redis_set_data
 from functions.exif import get_exif_data
 
-redis_client().set('foo', 'bar')
-
 directory = 'C:/Users/kvchm/Pictures/pictures/Ollie'
 
 for root, subdirs, files in os.walk(directory):
     for file in files:
         if not '.ini' in file:
             full_path = root + '/' + file
+            print(file)
+
             redis_data = redis_check_existing(full_path)
+
             if redis_data:
-                json_redis_data = json.loads(redis_data)
+                redis_metadata = json.loads(redis_data['metadata'])
+                redis_binary_file = redis_data['binary_file']
             else:
                 metadata = get_exif_data(full_path)
-                redis_set_data(full_path, metadata)
+                binary_file = open(full_path, 'rb').read()
+                redis_set_data(full_path, metadata, binary_file)
+
+            break
