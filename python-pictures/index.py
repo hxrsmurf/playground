35583cc2245725efe375afe6078b27f06320f3ce
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 10:00:40 -0500

    Image Meta Dict

--- python-pictures/index.py
+++ python-pictures/index.py
@@ -5,14 +5,18 @@ directory = 'C:/Users/kvchm/Pictures/pictures/Ollie'
 
 for root, subdirs, files in os.walk(directory):
     for file in files:
-
         if not '.ini' in file:
+            print(file)
             full_path = root + '/' + file
             with exiftool.ExifToolHelper() as et:
                 metadata = et.get_metadata(full_path)
 
             for data in metadata:
+                image_meta = []
                 for key, value in data.items():
-                    print(key, value)
+                    temp_meta = {
+                        key: value
+                    }
+                    image_meta.append(temp_meta)
 
-            break
+print(image_meta)
