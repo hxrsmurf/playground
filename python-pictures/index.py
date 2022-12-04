b895d6bf331f71b398a623a047ef311483b23067
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 16:09:37 -0500

    Add initialize.py

--- python-pictures/index.py
+++ python-pictures/index.py
@@ -6,7 +6,3 @@ from functions.list_files import list_files
 
 directory = 'C:/Users/kvchm/Pictures/pictures/'
 all_files = list_files(directory)
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    for file in all_files:
-        executor.submit(redis_set_data(file, file, file))
