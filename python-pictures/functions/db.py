d6b70d83c0a1077c303e4b089ec217b5bba5a898
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 10:27:20 -0500

    Add redis_check_existing and redis_set_data db functions

--- python-pictures/functions/db.py
+++ python-pictures/functions/db.py
@@ -1,7 +1,22 @@
 import redis
+import json
 
 def redis_client():
     return redis.Redis(
         host='localhost',
         port='6379'
     )
+
+def redis_check_existing(full_path):
+    print(f'Retrieving from Redis:', full_path)
+    return redis_client().get(full_path)
+
+def redis_set_data(full_path, metadata):
+    try:
+        print(f'Adding to Redis:', full_path)
+        redis_client().set(
+            full_path, json.dumps(metadata)
+        )
+        return True
+    except:
+        return False
