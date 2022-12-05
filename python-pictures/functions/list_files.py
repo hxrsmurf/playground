d2cee6acdfce9960c66c132059f9977b8f78619b
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-05 18:05:49 -0500

    Refactor list_files to get actual subdir files

--- python-pictures/functions/list_files.py
+++ python-pictures/functions/list_files.py
@@ -1,32 +1,29 @@
 import os
 
 def list_files(directory):
-    list_of_pictures = {}
+    list_of_files = {}
 
     for root, subdirs, files in os.walk(directory):
-        for subdir in subdirs:
-            subdir_pictures = []
+        folder_files = []
 
-            for file in files:
+        path = root.split(os.sep)
+        full_path = '/'.join(path)
+        folder = full_path.split(f'{directory}')[1]
+        subfolder = (folder.split('/'))[0]
+
+        for file in files:
+            try:
                 extension = file.split('.')[1]
                 if extension == 'jpg' or extension == 'JPG' or extension == 'png' or extension == 'PNG':
-
-                    full_path = root + '/' + subdir + '/' + file
-                    split_subdir = full_path.split(directory)[1]
-                    split_path = split_subdir.split(file)[0]
-                    parent_subdir = split_path.split('/')[0]
-
-                    if not parent_subdir:
-                        parent_subdir = split_path.split('/')[1]
-
-                    subdir_pictures.append({
+                    folder_files.append({
                         'full_path': full_path,
-                        'split_subdir': split_path,
-                        'parent_subdir': parent_subdir,
-                        'subdir': subdir,
+                        'folder': folder,
+                        'subfolder': subfolder,
                         'file': file
                     })
+            except Exception as e:
+                print(full_path, file, e)
+                pass
+        list_of_files[folder] = folder_files
 
-            list_of_pictures[parent_subdir] = subdir_pictures
-
-    return list_of_pictures
+    return list_of_files
--- python-pictures/index.py
+++ python-pictures/index.py
@@ -7,4 +7,7 @@ all_files = list_files(directory)
 for folder, files in all_files.items():
     in_redis = redis_check_existing(folder)
     if not in_redis:
+        print('Adding to Redis', folder)
         redis_set_data(folder, files)
+    else:
+        print('Exists in Redis', folder)
