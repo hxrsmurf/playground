9abcc1c566ae5c5acc94ceafb4047d560efc4daf
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 19:45:35 -0500

    Basic image listing

--- nextjs-pictures/app/page.tsx
+++ nextjs-pictures/app/page.tsx
@@ -1,10 +1,23 @@
-import { createClient } from 'redis'
-import { v4 as uuidv4 } from 'uuid'
+import { redis_album } from '../components/redis_client'
 
-export default function Home() {
+export default async function Home() {
+  const album = await redis_album('')
   return (
     <div className='flex justify-center mt-14'>
-      <div>Hello World</div>
+      <div>
+        <div>Album Name</div>
+        <div className='grid'>
+          {Object.entries(album).map((a: any, id: any) => (
+            <div key={id} className='mt-4 text-white'>
+              {a.map((image: any, id: any) => (
+                <div key={id}>
+                  <div>{image.file}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
