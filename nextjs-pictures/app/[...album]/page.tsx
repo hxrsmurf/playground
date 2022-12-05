72d0d676e2d7a656a9f525c8c5c2eaf3e3e0b7e8
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 20:21:07 -0500

    Add decode_uri and album not found

--- nextjs-pictures/app/[...album]/page.tsx
+++ nextjs-pictures/app/[...album]/page.tsx
@@ -1,23 +1,30 @@
 import { redis_album } from '../../components/redis_client'
 
 export default async function page({ params }: any) {
-  const album = params['album'][1]
+  const album = decodeURI(params['album'][1])
   const album_details = await redis_album(album)
+
   return (
     <div className='flex justify-center mt-14'>
       <div>
-        <div className='text-3xl font-bold mb-4'>{album}</div>
-        <div className='grid'>
-          {Object.entries(album_details).map((album: any, id: any) => (
-            <div key={id} className='mt-4 text-white'>
-              {album.map((image: any, id: any) => (
-                <div key={id}>
-                  <div>{image.file}</div>
+        {!album_details ? (
+          <div>Album Not Found</div>
+        ) : (
+          <>
+            <div className='text-3xl font-bold mb-4'>{album}</div>
+            <div className='grid'>
+              {Object.entries(album_details).map((album: any, id: any) => (
+                <div key={id} className='mt-4 text-white'>
+                  {album.map((image: any, id: any) => (
+                    <div key={id}>
+                      <div>{image.file}</div>
+                    </div>
+                  ))}
                 </div>
               ))}
             </div>
-          ))}
-        </div>
+          </>
+        )}
       </div>
     </div>
   )
