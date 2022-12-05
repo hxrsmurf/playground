c3ac9be0723883134d19dadf75e6b9d5aba8857c
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-04 20:21:16 -0500

    Add redis_get_all_albums

--- nextjs-pictures/app/page.tsx
+++ nextjs-pictures/app/page.tsx
@@ -1,19 +1,18 @@
-import { redis_album } from '../components/redis_client'
+import Link from 'next/link'
+import { redis_get_all_albums } from '../components/redis_client'
 
 export default async function Home() {
-  const album = await redis_album('')
+  const all_albums = await redis_get_all_albums()
   return (
     <div className='flex justify-center mt-14'>
       <div>
         <div>Album Name</div>
         <div className='grid'>
-          {Object.entries(album).map((a: any, id: any) => (
-            <div key={id} className='mt-4 text-white'>
-              {a.map((image: any, id: any) => (
-                <div key={id}>
-                  <div>{image.file}</div>
-                </div>
-              ))}
+          {all_albums.map((album: any, id: any) => (
+            <div key={id}>
+              <div>
+                <Link href={'album/' + album}>{album}</Link>
+              </div>
             </div>
           ))}
         </div>
