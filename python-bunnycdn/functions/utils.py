7aca2fc8486ce5d95c0e6c466b9518648ea28a89
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-13 19:20:37 -0500

    Update utils.py

--- python-bunnycdn/functions/utils.py
+++ python-bunnycdn/functions/utils.py
@@ -1,9 +1,4 @@
-import requests
 import json
-
-def create_video(bunny_cdn_url, headers, payload):
-    response = requests.post(bunny_cdn_url, data=payload, headers=headers)
-    return json.loads(response.text)['guid']
 
 def create_headers(api_key):
     headers = {
@@ -19,12 +14,6 @@ def create_payload(title, collectionId=None):
         'collectionId': collectionId
     }
     return json.dumps(payload)
-
-def upload_video(bunny_cdn_url, headers, guid, file):
-    video_url = f'{bunny_cdn_url}/{guid}'
-    file_bytes = open(file,'rb')
-    response = requests.put(video_url, headers=headers,data=file_bytes)
-    return response.status_code
 
 def create_title(title):
     split = title.split('[')[0]
