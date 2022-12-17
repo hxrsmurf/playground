e685dbd7a1e54acb575ab4fd11d9a7d11970a698
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-16 19:47:17 -0500

    Add new database lib Refactor redis db client Refactor status page for Redis

--- nextjs-spotify-public/pages/api/status/redis.ts
+++ nextjs-spotify-public/pages/api/status/redis.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { redis } from '../../../redis'
-
+import { redis } from '../../../lib/database'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   redis()
     .connect()
