8de29fdbe774660817d6a5cb577ffbcdcf9dca56
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-17 07:54:03 -0500

    Add catch for invalid year_month

--- nextjs-spotify-public/pages/[year-month].tsx
+++ nextjs-spotify-public/pages/[year-month].tsx
@@ -3,11 +3,20 @@ import {
   redis_get_tracks,
 } from '../components/database/get'
 import { redis_update_tracks } from '../components/database/update'
+import MonthNotAvailable from '../components/MonthNotAvailable'
+import { available_dates } from '../utils/dates'
 import { top_artists } from '../utils/top'
 import ChartArtists from './charts/top/artists'
 
 export default function YearMonth(props: any) {
   const year_month = props.year_month
+  const available_months = available_dates().months
+
+  if (!available_months.includes(year_month)) {
+    console.log(year_month)
+    return <MonthNotAvailable month={year_month} />
+  }
+
   const year_month_tracks = props.year_month_tracks
   const top_artists = props.artists
 
@@ -32,7 +41,7 @@ export default function YearMonth(props: any) {
       </div>
       <div className='flex justify-center mt-14'>
         <div className='min-h-[1200px] min-w-[1500px]'>
-          <ChartArtists data={top_artists} year_month={year_month}/>
+          <ChartArtists data={top_artists} year_month={year_month} />
         </div>
       </div>
     </>
@@ -41,6 +50,16 @@ export default function YearMonth(props: any) {
 
 export async function getServerSideProps(context: any) {
   const year_month = context.query['year-month']
+
+  // Catch if someone inputs the wrong date
+  if (year_month.length != 7) {
+    return {
+      props: {
+        year_month: year_month,
+      },
+    }
+  }
+
   let year_month_tracks = undefined
 
   year_month_tracks = await redis_get_tracks(year_month)
