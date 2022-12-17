fab433f0f66344690a87c91ed2f7909f77bb257c
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-16 20:06:25 -0500

    Add updated top artists (table)

--- nextjs-spotify-public/pages/[year-month].tsx
+++ nextjs-spotify-public/pages/[year-month].tsx
@@ -3,16 +3,29 @@ import {
   redis_get_tracks,
 } from '../components/database/get'
 import { redis_update_tracks } from '../components/database/update'
+import { top_artists } from '../utils/top'
 
 export default function YearMonth(props: any) {
+  const year_month = props.year_month
   const year_month_tracks = props.year_month_tracks
+  const top_artists = props.artists
 
   if (!year_month_tracks) return <>Invalid Page</>
 
   return (
     <div className='flex justify-center mt-14'>
       <div className='grid grid-flow-row'>
-        <div>Hello World</div>
+        <div>Top 10 Artists - {year_month}</div>
+        <div className='grid grid-flow-col my-4 font-bold'>
+          <div className='min-w-[150px]'>Artist</div>
+          <div>Count</div>
+        </div>
+        {Object.keys(top_artists).map((artist: string, id) => (
+          <div key={id} className='grid grid-flow-col'>
+            <div className='min-w-[150px]'>{artist}</div>
+            <div>{top_artists[artist]}</div>
+          </div>
+        ))}
       </div>
     </div>
   )
@@ -36,7 +49,9 @@ export async function getServerSideProps(context: any) {
     console.log('Using Redis')
   }
 
+  const artists = top_artists(year_month_tracks)
+
   return {
-    props: { year_month_tracks }, // will be passed to the page component as props
+    props: { year_month, year_month_tracks, artists }, // will be passed to the page component as props
   }
 }
---
+++ nextjs-spotify-public/utils/top.ts
@@ -0,0 +1,24 @@
+export function top_artists(tracks: any) {
+  let play_count: any = {}
+
+  tracks.forEach((element: any) => {
+    const song_id = element['songID']['S']
+    const song = element['song']['S']
+    const album = element['album']['S']
+    const artist = element['artist']['S']
+    const device = element['device']['S']
+    const repeat = element['possibleDuplicate']['BOOL']
+
+    if (!repeat) {
+      play_count[artist] = play_count[artist] + 1 || 1
+    }
+  })
+
+  const artists = Object.fromEntries(
+    Object.entries(play_count)
+      .sort(([, a]: any, [, b]: any) => b - a)
+      .slice(0, 10)
+  )
+
+  return artists
+}
