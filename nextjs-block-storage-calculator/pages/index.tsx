ea7f6557f71b0471173438406dcaab212761214d
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-30 15:30:25 -0500

    Add cloudflare

--- nextjs-block-storage-calculator/pages/index.tsx
+++ nextjs-block-storage-calculator/pages/index.tsx
@@ -1,9 +1,10 @@
 import Backblaze from "../components/backblaze";
 import BunnyCDN from "../components/bunnycdn";
+import Cloudflare from "../components/cloudflare";
 
 export default function Home() {
   return (
-    <div className='flex justify-center mt-8'>
+    <div className='flex justify-center mt-8 mb-24'>
       <div className="min-w-[800px]">
         <div>Hello World</div>
         <div className="mt-4">
@@ -12,6 +13,10 @@ export default function Home() {
         <div className="min-h-[2px] bg-white m-8"></div>
         <div className="mt-4">
           <BunnyCDN/>
+        </div>
+        <div className="min-h-[2px] bg-white m-8"></div>
+        <div className="mt-4">
+          <Cloudflare/>
         </div>
       </div>
     </div>
