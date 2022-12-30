a64996f554c9d05bd801484d21164271ccfb18d7
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-30 16:37:44 -0500

    Add calculator

--- nextjs-block-storage-calculator/pages/index.tsx
+++ nextjs-block-storage-calculator/pages/index.tsx
@@ -1,6 +1,7 @@
 import AWS from "../components/aws";
 import Backblaze from "../components/backblaze";
 import BunnyCDN from "../components/bunnycdn";
+import Calculator from "../components/calculator";
 import Cloudflare from "../components/cloudflare";
 import DigitalOcean from "../components/digitalocean";
 import OVH from "../components/ovh";
@@ -9,7 +10,9 @@ export default function Home() {
   return (
     <div className='flex justify-center mt-8 mb-24'>
       <div className="min-w-[800px]">
-        <div>Hello World</div>
+        <div className="mb-24">
+          <Calculator/>
+        </div>
         <div className="mt-4">
           <Backblaze/>
         </div>
