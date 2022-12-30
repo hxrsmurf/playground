6f4b2bb2016d6f0c2112d9f1aa0b1bd21b284852
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-30 15:15:03 -0500

    Add bunnycdn

---
+++ nextjs-block-storage-calculator/components/bunnycdn.tsx
@@ -0,0 +1,104 @@
+import Link from 'next/link'
+
+const pricing_info = [
+  {
+    name: 'Single Region',
+    price: '$0.01/GB',
+  },
+  {
+    name: 'Two Regions',
+    price: '$0.02/GB',
+  },
+  {
+    name: 'Three Regions',
+    price: '$0.025/GB',
+  },
+  {
+    name: 'Each Additional Region',
+    price: '+$0.005/GB',
+  },
+]
+
+const pricing_cdn = [
+  {
+    name: 'Europe & North America',
+    price: '$0.01/GB',
+  },
+  {
+    name: 'Asia & Oceania',
+    price: '$0.03/GB',
+  },
+  {
+    name: 'South America',
+    price: '$0.045/GB',
+  },
+  {
+    name: 'Middle East & Africa',
+    price: '$0.06/GB',
+  },
+]
+
+const pricing_stream = [
+  {
+    name: 'Encoding',
+    price: 'Free',
+  },
+  {
+    name: 'Transcoding',
+    price: 'Free',
+  },
+  {
+    name: 'Storage',
+    price: 'From $0.01/GB',
+  },
+  {
+    name: 'CDN',
+    price: 'From $0.005/GB',
+  },
+]
+
+export default function BunnyCDN() {
+  return (
+    <div>
+      <div>
+        <Link href={'https://bunny.net/pricing/'}>Bunny CDN Pricing</Link>
+      </div>
+
+      <div className='mt-4 font-bold text-2xl'>Storage</div>
+      <div>
+        {pricing_info.map((category, id) => (
+          <div key={id} className='grid grid-flow-col mt-4'>
+            <div className='min-w-[200px] max-w-[200px]'>{category.name}</div>
+            <div className='min-w-[200px] max-w-[200px] flex justify-center'>
+              <div>{category.price}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='mt-6 font-bold text-2xl'>CDN</div>
+      <div>
+        {pricing_cdn.map((category, id) => (
+          <div key={id} className='grid grid-flow-col mt-4'>
+            <div className='min-w-[200px] max-w-[200px]'>{category.name}</div>
+            <div className='min-w-[200px] max-w-[200px] flex justify-center'>
+              <div>{category.price}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='mt-6 font-bold text-2xl'>Stream</div>
+      <div>
+        {pricing_stream.map((category, id) => (
+          <div key={id} className='grid grid-flow-col mt-4'>
+            <div className='min-w-[200px] max-w-[200px]'>{category.name}</div>
+            <div className='min-w-[200px] max-w-[200px] flex justify-center'>
+              <div>{category.price}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- nextjs-block-storage-calculator/pages/index.tsx
+++ nextjs-block-storage-calculator/pages/index.tsx
@@ -1,4 +1,5 @@
 import Backblaze from "../components/backblaze";
+import BunnyCDN from "../components/bunnycdn";
 
 export default function Home() {
   return (
@@ -7,6 +8,10 @@ export default function Home() {
         <div>Hello World</div>
         <div className="mt-4">
           <Backblaze/>
+        </div>
+        <div className="min-h-[2px] bg-white m-8"></div>
+        <div className="mt-4">
+          <BunnyCDN/>
         </div>
       </div>
     </div>
