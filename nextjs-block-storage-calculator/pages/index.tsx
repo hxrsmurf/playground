0c0f36d094b161f6bbb5f005a6d7d0c9ea767d7c
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-30 14:59:08 -0500

    Add Backblaze component

--- nextjs-block-storage-calculator/pages/index.tsx
+++ nextjs-block-storage-calculator/pages/index.tsx
@@ -1,8 +1,13 @@
+import Backblaze from "../components/backblaze";
+
 export default function Home() {
   return (
     <div className='flex justify-center mt-8'>
-      <div>
+      <div className="min-w-[800px]">
         <div>Hello World</div>
+        <div className="mt-4">
+          <Backblaze/>
+        </div>
       </div>
     </div>
   )
