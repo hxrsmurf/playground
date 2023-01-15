50aaf0d118bc64250e20803dd7a24e88fadb2aac
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-15 23:12:07 +0000

    Change size of textarea

--- nextjs-journal/pages/components/Form.tsx
+++ nextjs-journal/pages/components/Form.tsx
@@ -5,8 +5,8 @@ export default function Form() {
 
     return (
         <div className="m-4">
-            <div>{current_date}</div>
-            <div className="mt-4"><textarea></textarea></div>
+            <div className="text-2xl font-bold">{current_date}</div>
+            <div className="mt-4"><textarea className="min-h-[400px] min-w-[400px]"></textarea></div>
             <div className="mt-4 rounded-full bg-blue-600 p-2 text-center max-w-[400px]"><button>Submit</button></div>
         </div>
     )
