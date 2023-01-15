f4df7e0c70c3e03f43771a79b65a3f999f0d13ce
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-14 21:32:06 -0500

    Remove extra makes

--- electron-testing/forge.config.js
+++ electron-testing/forge.config.js
@@ -6,17 +6,5 @@ module.exports = {
       name: '@electron-forge/maker-squirrel',
       config: {},
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
   ],
 };
