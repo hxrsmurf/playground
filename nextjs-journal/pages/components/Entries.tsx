030c6f05b79dba625d1260c3d9d87c318d5477e6
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-15 23:46:33 +0000

    Add dynamic entry page

--- nextjs-journal/pages/components/Entries.tsx
+++ nextjs-journal/pages/components/Entries.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link"
+
+const list_entries = [
+    {
+        id: 1,
+        title: 'My Title 1',
+        text: 'Blah blah blah 1'
+
+    },
+    {
+        id: 2,
+        title: 'My Title 2',
+        text: 'Blah blah blah 2'
+    }
+]
+
 export default function Entries() {
     return (
         <div className="max-w-[100px] space-y-4 mt-4 min-[800px]:min-w-[200px] min-[800px]:pr-5">
@@ -10,6 +26,14 @@ export default function Entries() {
             <div>
                 Sun, 15 Jan 2023 23:25:14 GMT
             </div>
+            {list_entries.map((entry, id) => (
+                <div key={id}>
+                    <Link href={'/entry/' + entry.id}>
+                        {entry.text}
+                    </Link>
+
+                </div>
+            ))}
         </div>
     )
 }
