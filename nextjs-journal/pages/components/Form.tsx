21c96fae151dfa761af40a80ed3016e54307f683
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-15 23:09:32 +0000

    Add date util function

--- nextjs-journal/pages/components/Form.tsx
+++ nextjs-journal/pages/components/Form.tsx
@@ -1,9 +1,13 @@
+import { date } from "../utils/utils"
+
 export default function Form() {
-  return (
-    <div className="m-4">
-        <div>Title</div>
-        <div className="mt-4"><textarea></textarea></div>
-        <div className="mt-4 rounded-full bg-blue-600 p-2 text-center max-w-[400px]"><button>Submit</button></div>
-    </div>
-  )
+    const current_date = date().toUTCString()
+
+    return (
+        <div className="m-4">
+            <div>{current_date}</div>
+            <div className="mt-4"><textarea></textarea></div>
+            <div className="mt-4 rounded-full bg-blue-600 p-2 text-center max-w-[400px]"><button>Submit</button></div>
+        </div>
+    )
 }
