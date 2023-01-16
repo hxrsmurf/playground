05c14f35ada9fc418e9d23b5269fd4bc831a1a45
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-16 23:13:00 +0000

    Add GeneraeteUrl Function

---
+++ python-spotify/code/generate-url/index.py
@@ -0,0 +1,20 @@
+import logging
+
+from functions.generate import generate_url
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+
+    generated_url = generate_url()
+
+    html = f"<html><a href='{generated_url}'>{generated_url}</a></html>"
+
+    return ({
+        'statusCode': 200,
+        'body': html,
+        'headers': {
+            'Content-Type': 'text/html; charset=utf-8'
+        }
+    })
