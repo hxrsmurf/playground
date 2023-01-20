56435217aa033274075f8d3811a04f059dd72a8c
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-20 00:32:20 +0000

    Add update_item function

--- python-spotify/code/refresh-token/functions/dynamodb.py
+++ python-spotify/code/refresh-token/functions/dynamodb.py
@@ -10,3 +10,24 @@ def scan_table():
     )
 
     return response['Items']
+
+def update_item(id, access_token, refresh_token):
+    response = client.update_item(
+        TableName = os.environ['Table'],
+        Key = {
+            'id': {
+                'S': id
+            },
+        },
+        UpdateExpression = "SET access_token=:access_token, refresh_token=:refresh_token",
+        ExpressionAttributeValues = {
+            ":access_token": {
+                'S': access_token
+            },
+            ":refresh_token": {
+                'S': refresh_token
+            }
+        }
+    )
+
+    logging.info(response)
--- python-spotify/code/refresh-token/index.py
+++ python-spotify/code/refresh-token/index.py
@@ -1,5 +1,5 @@
 import logging
-from functions.dynamodb import scan_table
+from functions.dynamodb import scan_table, update_item
 from functions.parse import parse_scan_table
 from functions.me import me
 from functions.tokens import get_refresh_token
@@ -10,13 +10,16 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     parse_results = parse_scan_table(scan_table())
     for user in parse_results:
+        id = user['id']
         access_token = user['access_token']
         refresh_token = user['refresh_token']
 
         profile = me(access_token)
 
         if not profile:
-            print('Getting new token')
+            print('Getting new refresh token')
             access_token, refresh_token, token_type = get_refresh_token(refresh_token)
             profile = me(access_token)
-            print(profile)
+            update_item(id, access_token, refresh_token)
+
+        print(profile)
