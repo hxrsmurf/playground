a2b4f077e31d682335a535ed1e7d1667d453ef03
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 08:29:56 +0000

    Initial lambda function

---
+++ google-spaces/aws/lambda.tf
@@ -0,0 +1,74 @@
+data "archive_file" "archive" {
+  type             = "zip"
+  source_dir = "./code"
+  output_file_mode = "0666"
+  output_path      = "./${var.name}.zip"
+}
+
+resource "aws_iam_role" "role" {
+    assume_role_policy    = jsonencode(
+        {
+            Statement = [
+                {
+                    Action    = "sts:AssumeRole"
+                    Effect    = "Allow"
+                    Principal = {
+                        Service = "lambda.amazonaws.com"
+                    }
+                },
+            ]
+            Version   = "2012-10-17"
+        }
+    )
+
+    managed_policy_arns   = var.policy-arn
+
+    name                  = "${var.name}-tf"
+    path                  = "/"
+}
+
+resource "aws_lambda_function" "function" {
+    architectures                  = [
+        "x86_64",
+    ]
+    function_name                  = "${var.name}-tf"
+    handler                        = "index.handler"
+
+    layers                         = var.layers
+
+    memory_size                    = var.memory
+    package_type                   = "Zip"
+    reserved_concurrent_executions = -1
+    role                           = aws_iam_role.role.arn
+    runtime                        = var.runtime
+    filename                       = data.archive_file.archive.output_path
+    source_code_hash               = filebase64sha256(data.archive_file.archive.output_path)
+
+    timeout                        = var.timeout
+
+    environment {
+        variables = var.environment
+    }
+
+    ephemeral_storage {
+        size = 512
+    }
+
+    timeouts {}
+
+    tracing_config {
+        mode = "PassThrough"
+    }
+}
+
+output "arn" {
+    value = aws_lambda_function.function.arn
+}
+
+output "invoke_arn" {
+    value = aws_lambda_function.function.invoke_arn
+}
+
+output "url" {
+    value = "https://us-east-1.console.aws.amazon.com/lambda/home?region=us-east-1#/functions/${aws_lambda_function.function.id}"
+}
---
+++ google-spaces/aws/variables.tf
@@ -0,0 +1,37 @@
+variable "name" {
+  default = "google-spaces"
+}
+
+variable "layers" {
+  default = [
+    "arn:aws:lambda:us-east-1:195663387853:layer:requests-tf:8"
+  ]
+}
+
+variable "memory" {
+  default = "256"
+}
+
+variable "timeout" {
+  default = "30"
+}
+
+variable "runtime" {
+  default = "python3.9"
+}
+
+variable "environment" {
+  Space = "test"
+  Key = "test"
+  Token = "test"
+}
+
+variable "policy-arn" {
+  default = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+}
+
+variable "log-retention-days" {
+  default = 3
+}
