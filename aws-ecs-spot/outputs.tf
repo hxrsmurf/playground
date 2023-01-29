2dc59ecd7ebda8acd954eae37036767e7aa2d5a8
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-29 04:11:24 +0000

    Add outputs.tf

---
+++ aws-ecs-spot/outputs.tf
@@ -0,0 +1,7 @@
+output "cli"  {
+    value = {
+        step-1 = "aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com"
+        step-2 = "docker tag nextjs-spotify-public:latest ${aws_ecr_repository.testing.repository_url}:latest"
+        step-3 = "docker push ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com/${var.name}:latest"
+    }
+}
