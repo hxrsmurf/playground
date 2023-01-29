4e8474f8b4278a24e5e3f2a4863a0d6f90844f84
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-29 06:04:01 +0000

    Add TF Cloud

--- aws-ecs-spot/provider.tf
+++ aws-ecs-spot/provider.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
     region = "us-east-1"
 }
+
+terraform {
+  cloud {
+    organization = "nextjs-spotify-public"
+
+    workspaces {
+      name = "nextjs-spotify-public"
+    }
+  }
+}
