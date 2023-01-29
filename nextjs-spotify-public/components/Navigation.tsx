f94b8ea06ec8300e018236b8e1dcbf3289071390
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-29 13:07:57 -0500

    nextjs-spotify-public - Add mobile view (#30)

--- nextjs-spotify-public/components/Navigation.tsx
+++ nextjs-spotify-public/components/Navigation.tsx
@@ -7,16 +7,34 @@ export default function Navigation() {
 
   return (
     <div className='flex justify-center mt-4'>
-      <div className='grid grid-flow-col space-x-4'>
-        {/* <div>
-          <Link href={years}>{years}</Link>
-        </div> */}
+      {/* Desktop Menu */}
+      <div className='hidden min-[1400px]:grid min-[1400px]:grid-flow-col min-[1400px]:space-x-4'>
         {Object.values(months).map((month: any, id: any) => (
           <div key={id}>
             <Link href={month}>{month}</Link>
           </div>
         ))}
       </div>
+      {/* Desktop Menu */}
+
+      {/* Mobile Menu */}
+      <div className='cursor-pointer min-[1400px]:hidden'>
+        <details>
+          <summary>Menu</summary>
+          <div>
+            {/* <div>
+              <Link href={years}>{years}</Link>
+            </div> */}
+            {Object.values(months).map((month: any, id: any) => (
+              <div key={id}>
+                <Link href={month}>{month}</Link>
+              </div>
+            ))}
+          </div>
+        </details>
+      </div>
+      {/* Mobile Menu */}
+
     </div>
   )
 }
