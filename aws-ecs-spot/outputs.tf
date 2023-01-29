ea989b63eacf5b81b599c60293af318a3eee7026
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-29 07:05:25 +0000

    Update outputs.tf to build and tag

--- aws-ecs-spot/outputs.tf
+++ aws-ecs-spot/outputs.tf
@@ -1,16 +1,18 @@
 output "cli"  {
     value = {
         step-1 = "aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com"
-        step-2 = "docker tag nextjs-spotify-public:latest ${aws_ecr_repository.testing.repository_url}:latest"
-        step-3 = "docker push ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com/${var.name}:latest"
+        step-2 = "docker build . -t nextjs-spotify-public:latest -t ${aws_ecr_repository.testing.repository_url}:latest"
+        step-3 = "docker tag nextjs-spotify-public:latest ${aws_ecr_repository.testing.repository_url}:latest"
+        step-5 = "docker push ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com/${var.name}:latest"
     }
 }
 
 resource "local_file" "build-sh" {
   filename = "build.sh"
   content  = <<EOT
-aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com
-docker tag nextjs-spotify-public:latest ${aws_ecr_repository.testing.repository_url}:latest
-docker push ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com/${var.name}:latest
+aws ecr get-login-password --region ${var.region} | sudo docker login --username AWS --password-stdin ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com
+sudo docker build . -t nextjs-spotify-public:latest -t ${aws_ecr_repository.testing.repository_url}:latest
+# sudo docker tag nextjs-spotify-public:latest ${aws_ecr_repository.testing.repository_url}:latest
+sudo docker push ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com/${var.name}:latest
   EOT
 }
