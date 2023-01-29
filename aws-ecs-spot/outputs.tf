7c9561b7f855679df7a198b46a8873a2711b223a
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-29 09:06:51 -0500

    nextjs-spotify-public - Add Build Arg for Cache (#29)

--- aws-ecs-spot/outputs.tf
+++ aws-ecs-spot/outputs.tf
@@ -11,7 +11,7 @@ resource "local_file" "build-sh" {
   filename = "build.sh"
   content  = <<EOT
 aws ecr get-login-password --region ${var.region} | sudo docker login --username AWS --password-stdin ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com
-sudo docker build . -t nextjs-spotify-public:latest -t ${aws_ecr_repository.testing.repository_url}:latest
+sudo docker build . -t nextjs-spotify-public:latest -t ${aws_ecr_repository.testing.repository_url}:latest --build-arg CACHE=1
 # sudo docker tag nextjs-spotify-public:latest ${aws_ecr_repository.testing.repository_url}:latest
 sudo docker push ${var.account-id}.dkr.ecr.${var.region}.amazonaws.com/${var.name}:latest
   EOT
