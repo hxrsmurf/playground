efa351a8dc6cf115bfb33eff5dee085c07fb8d36
Kevin Murphy <kevin@hxrsmurf.info>
2023-02-01 00:19:23 +0000

    Import new function into index

--- nextjs-spotify-public/aws/list-channels/code/index.py
+++ nextjs-spotify-public/aws/list-channels/code/index.py
@@ -1,16 +1,16 @@
 import json
 
 from functions.ec2 import describe_instances
+from functions.cloudflare import verify
 
 def lambda_handler(event, context):
     record = event['Records'][0]
     sns_message = record['Sns']
     subject = sns_message['Subject']
     message = sns_message['Message']
-    
+
     if subject == 'Auto Scaling: launch':
         instance_id = message['EC2InstanceId']
         instance_information = describe_instances(instance_id)['Reservations'][0]['Instances'][0]
         public_ip = instance_information['PublicIpAddress']
         print(public_ip)
-        
