2864ac4ed1eb15d1a999855ff2ded85074ce13a0
Kevin Murphy <kevin@hxrsmurf.info>
2023-03-12 21:51:24 -0400

    Update index.py to parse event bridge

--- nextjs-spotify-public/aws/update-cloudflare/code/index.py
+++ nextjs-spotify-public/aws/update-cloudflare/code/index.py
@@ -4,16 +4,12 @@ from functions.ec2 import describe_instances
 from functions.cloudflare import update_dns_record
 
 def lambda_handler(event, context):
-    # Parse SNS Message
-    record = event['Records'][0]
-    sns_message = record['Sns']
-    subject = sns_message['Subject']
-    message = json.loads(sns_message['Message'])
-
-    if subject == 'Auto Scaling: launch':
+    # Parse Auto Scaling Event
+    print(event)
+    run = True
+    if run:
         # Parse EC2 Information
-        instance_id = message['EC2InstanceId']
+        instance_id = event['detail']['EC2InstanceId']
         instance_information = describe_instances(instance_id)['Reservations'][0]['Instances'][0]
         public_ip = instance_information['PublicIpAddress']
-
         update_dns_record(ip_address=public_ip)
