18b26f51fd9edc5004a425f4d1d31fce8c08b900
Kevin Murphy <kevin@hxrsmurf.info>
2023-04-16 13:15:45 +0000

    Add IAM GetRole

--- movies-tv/github.tf
+++ movies-tv/github.tf
@@ -1,28 +1,44 @@
 resource "aws_iam_role" "iam_for_github" {
-    assume_role_policy    = jsonencode(
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
         {
-            Statement = [
-                {
-                    Action    = "sts:AssumeRoleWithWebIdentity"
-                    Condition = {
-                        StringEquals = {
-                            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-                        }
-                    }
-                    Effect    = "Allow"
-                    Principal = {
-                        Federated = "arn:aws:iam::195663387853:oidc-provider/token.actions.githubusercontent.com"
-                    }
-                },
-            ]
-            Version   = "2012-10-17"
-        }
+          Action = "sts:AssumeRoleWithWebIdentity"
+          Condition = {
+            StringEquals = {
+              "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            }
+          }
+          Effect = "Allow"
+          Principal = {
+            Federated = "arn:aws:iam::195663387853:oidc-provider/token.actions.githubusercontent.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  description          = "GitHub role for playground-repo/movies-tv"
+  managed_policy_arns  = []
+  max_session_duration = 3600
+  name                 = "movies-tv-github"
+  path                 = "/"
+
+  inline_policy {
+    name = "IAM"
+    policy = jsonencode(
+      {
+        Statement = [
+          {
+            Action   = "iam:GetRole"
+            Effect   = "Allow"
+            Resource = "arn:aws:iam::195663387853:role/movies-tv-github"
+          },
+        ]
+        Version = "2012-10-17"
+      }
     )
-    description           = "GitHub role for playground-repo/movies-tv"
-    managed_policy_arns   = []
-    max_session_duration  = 3600
-    name                  = "movies-tv-github"
-    path                  = "/"
+  }
 }
 
 output "iam_for_github" {
