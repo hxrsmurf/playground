74f98e99ca862fa9ade3497d8087af1ff6b7f5df
Kevin <kevin@hxrsmurf.info>
2023-06-18 13:14:27 -0400

    Parse obsidian results

--- nextjs-journal/app/[year]/[month]/[day]/page.tsx
+++ nextjs-journal/app/[year]/[month]/[day]/page.tsx
@@ -1,6 +1,12 @@
 import { currentUser } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/dist/types/server'
 import Link from 'next/link'
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  PromiseLikeOfReactNode,
+} from 'react'
 import { createClient } from 'redis'
 
 async function fetchEntries(user: User | null, year_month_day: string) {
@@ -8,7 +14,8 @@ async function fetchEntries(user: User | null, year_month_day: string) {
     url: 'redis://' + process.env.REDIS_SERVER + ':6379',
   })
   await client.connect()
-  const redis_key: string = user!['id'] + "-" + year_month_day
+  const redis_key: string =
+    user!['id'] + '-' + year_month_day.replace('/', '-').replace('/', '-')
   const entries = await client.sScan(redis_key, 0)
   return entries['members']
 }
@@ -26,16 +33,28 @@ export default async function Page({
   const user = await currentUser()
   const entries = await fetchEntries(user, year_month_day)
 
+  var content: any = null
+
+  try {
+    content = JSON.parse(entries[0]).content
+  } catch (error) {}
+
   return (
     <div className='flex justify-center mt-14'>
       <div className='font-bold'>
         <Link href={'/'}>Home</Link>
         <div className='font-bold mt-4'>
-          Journal for {year}-{month}-{day}
+          Journal for {year}-
+          <Link href={'/' + year + '/' + month}>{month}</Link>-{day}
           {entries.map((entry, key) => (
             <div key={key} className='mt-4'>
-              <div>{new Date(JSON.parse(entry).timestamp).toString()}</div>
-              <div>{JSON.parse(entry).entry}</div>
+              {/* <div>{new Date(JSON.parse(entry).timestamp).toString()}</div> */}
+              {/* <div>{JSON.parse(entry).entry}</div> */}
+              {content ? (
+                <div className='whitespace-pre-line font-normal'>{content}</div>
+              ) : (
+                <div>No Entry</div>
+              )}
             </div>
           ))}
         </div>
