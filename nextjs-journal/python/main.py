d927fb7d230ec6e7f09376ec749527c0af96bf43
Kevin <kevin@hxrsmurf.info>
2023-06-18 12:42:25 -0400

    format date

--- nextjs-journal/python/main.py
+++ nextjs-journal/python/main.py
@@ -1,4 +1,13 @@
 import os
+import datetime
+
+
+def remove_extension(file):
+    trim = file.split(' ')
+    only_date = trim[0]
+    formatted_date = datetime.datetime.strptime(
+        only_date, "%m-%d-%Y").strftime('%Y-%m-%d')
+    return formatted_date
 
 
 def main():
@@ -12,15 +21,16 @@ def main():
         if '.md' in file and not 'Huel' in file:
             markdown_files.append(file)
 
+    all_content = []
     for markdown in markdown_files:
         path = obsidian + '\\' + markdown
         with open(path, 'r') as file:
             output = file.read()
-            print({
-                'file': markdown,
+            data = {
+                'file': remove_extension(markdown),
                 'content': output
-            })
-            break
+            }
+            all_content.append(data)
 
 
 if __name__ == "__main__":
