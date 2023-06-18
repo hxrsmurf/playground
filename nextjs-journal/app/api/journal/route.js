6f8985465125adcc970f3fd6e46d62e0e20e2491
Kevin <kevin@hxrsmurf.info>
2023-06-18 11:39:45 -0400

    Add simple authentication via headers or param

--- nextjs-journal/app/api/journal/route.js
+++ nextjs-journal/app/api/journal/route.js
@@ -1,5 +1,7 @@
 import { todayPage } from '@/lib/utils'
 import { NextResponse } from 'next/server'
+import { headers } from 'next/headers'
+
 import { createClient } from 'redis'
 export async function POST(request) {
   const client = createClient({
@@ -15,15 +17,37 @@ export async function POST(request) {
 }
 
 export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const headersList = headers()
+  const authorization = headersList.get('authorization')
+  const api_key_params = searchParams.get('api')
+  var api_key = null
+
+  if (authorization) {
+    api_key = authorization
+  } else {
+    api_key = api_key_params
+  }
+
+  console.log(api_key)
+
   const client = createClient({
     url: 'redis://' + process.env.REDIS_SERVER + ':6379',
     legacyMode: false,
   })
+
   await client.connect()
 
-  const user = 'test'
+  const user = await client.get(api_key)
+
+  if (!user) return new Response('Not valid API Key')
+
   const testing = await client.sScan(user, 0)
-  return new Response(JSON.stringify(testing['members']), {
+
+  const list_of_entries = []
+  testing['members'].map((entry) => list_of_entries.push(JSON.parse(entry)))
+
+  return new Response(JSON.stringify(list_of_entries), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
