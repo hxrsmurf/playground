8f377ea6bee6917f97eb58d905277e9d1305f9c4
Kevin <kevin@hxrsmurf.info>
2023-06-18 11:15:03 -0400

    Add individual value returns for todayPage

--- nextjs-journal/lib/utils.js
+++ nextjs-journal/lib/utils.js
@@ -1,8 +1,17 @@
-export function todayPage(){
+import moment from 'moment'
+
+export function todayPage() {
   const today = new Date().toISOString().slice(0, 10)
   const year = today.split('-')[0]
   const month = today.split('-')[1]
   const day = today.split('-')[2]
+  const weekday_name = moment().format('dddd')
   const today_page = year + '/' + month + '/' + day
-  return today_page
+  return {
+    year: year,
+    month: month,
+    day: day,
+    today_page: today_page,
+    weekday: weekday_name,
+  }
 }
