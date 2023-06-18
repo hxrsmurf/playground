2c6f2fa5fb90ed21d519fd80c812869fbb64034b
Kevin <kevin@hxrsmurf.info>
2023-06-18 08:24:21 -0400

    Install redis

--- nextjs-journal/app/page.tsx
+++ nextjs-journal/app/page.tsx
@@ -2,10 +2,19 @@ import JournalForm from '@/components/JournalForm'
 import { todayPage } from '@/lib/utils'
 import { currentUser } from '@clerk/nextjs'
 import Link from 'next/link'
+import { createClient } from 'redis'
 
 export default async function Home() {
   const user = await currentUser()
   const today_page = todayPage()
+
+  const redis_server = "redis://" + process.env.REDIS_SERVER + ":6379"
+  const client = createClient({
+    url: redis_server
+  })
+
+  client.on('error', err => console.log('Redis Client Error', err))
+  await client.connect()
 
   return (
     <div className='flex justify-center mt-14'>
