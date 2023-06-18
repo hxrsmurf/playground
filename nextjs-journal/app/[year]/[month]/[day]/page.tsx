88aebf5333e19cf91cd1c233711ff788f9d1f6d2
Kevin <kevin@hxrsmurf.info>
2023-06-18 15:52:28 -0400

    Update individual page to reference latest redis

--- nextjs-journal/app/[year]/[month]/[day]/page.tsx
+++ nextjs-journal/app/[year]/[month]/[day]/page.tsx
@@ -1,23 +1,17 @@
 import { currentUser } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/dist/types/server'
 import Link from 'next/link'
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  PromiseLikeOfReactNode,
-} from 'react'
 import { createClient } from 'redis'
 
 async function fetchEntries(user: User | null, year_month_day: string) {
   const client = createClient({
     url: 'redis://' + process.env.REDIS_SERVER + ':6379',
   })
+
   await client.connect()
-  const redis_key: string =
-    user!['id'] + '-' + year_month_day.replace('/', '-').replace('/', '-')
-  const entries = await client.sScan(redis_key, 0)
-  return entries['members']
+  const user_id = user!['id']
+  const results = await client.hGet(user_id, year_month_day)
+  return results
 }
 export default async function Page({
   params,
@@ -29,36 +23,36 @@ export default async function Page({
   const month = params.month
   const day = params.day
 
-  const year_month_day = year + '/' + month + '/' + day
+  const year_month_day = year + '-' + month + '-' + day
   const user = await currentUser()
-  const entries = await fetchEntries(user, year_month_day)
+  const entries: any = await fetchEntries(user, year_month_day)
 
   var content: any = null
 
-  try {
-    content = JSON.parse(entries[0]).content
-  } catch (error) {}
+  if (entries) content = JSON.parse(entries).content
 
   return (
     <div className='flex justify-center mt-14'>
-      <div className='font-bold'>
-        <Link href={'/'}>Home</Link>
-        <div className='font-bold mt-4'>
-          Journal for {year}-
-          <Link href={'/' + year + '/' + month}>{month}</Link>-{day}
-          {entries.map((entry, key) => (
-            <div key={key} className='mt-4'>
-              {/* <div>{new Date(JSON.parse(entry).timestamp).toString()}</div> */}
-              {/* <div>{JSON.parse(entry).entry}</div> */}
-              {content ? (
-                <div className='whitespace-pre-line font-normal'>{content}</div>
-              ) : (
-                <div>No Entry</div>
-              )}
-            </div>
-          ))}
+      <div>
+        <div>
+          <Link className='font-bold' href={'/'}>
+            Home
+          </Link>
         </div>
-      </div>
+        <div>
+          Journal for <Link href={'/' + year}>{year}</Link> {' --> '}
+          <Link href={'/' + year + '/' + month}>{month}</Link>
+          {' --> '}
+          {day}
+        </div>
+        <div className='mt-4'>
+          {content ? (
+            <div className='whitespace-pre-line'>{content}</div>
+          ) : (
+            <>No entries</>
+          )}
+        </div>
+      </div>{' '}
     </div>
   )
 }
