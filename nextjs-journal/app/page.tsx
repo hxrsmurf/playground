3c8facc9da77195838268fb9fa77f22a50f49195
Kevin <kevin@hxrsmurf.info>
2023-06-18 06:48:25 -0400

    create today page util

--- nextjs-journal/app/page.tsx
+++ nextjs-journal/app/page.tsx
@@ -1,22 +1,18 @@
 import JournalForm from '@/components/JournalForm'
+import { todayPage } from '@/lib/utils'
 import { currentUser } from '@clerk/nextjs'
 import Link from 'next/link'
 
 export default async function Home() {
   const user = await currentUser()
-  const today = new Date().toISOString().slice(0, 10)
-
-  const year = today.split('-')[0]
-  const month = today.split('-')[1]
-  const day = today.split('-')[2]
-  const today_page = year + '/' + month + '/' + day
+  const today_page = todayPage()
 
   return (
     <div className='flex justify-center mt-14'>
       <div>
         <div>Welcome {user?.firstName}</div>
         <div>
-          Today is <Link href={today_page}>{today}</Link>
+          Today is <Link href={today_page}>{today_page}</Link>
         </div>
         <JournalForm />
       </div>
