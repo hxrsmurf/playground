48e28e57b31ed20a4046aa0065fdca9557a6ce79
Kevin Murphy <kevin@hxrsmurf.info>
2023-06-18 09:52:59 +0000

    Add today date

--- nextjs-journal/app/page.tsx
+++ nextjs-journal/app/page.tsx
@@ -1,11 +1,13 @@
 import { currentUser } from '@clerk/nextjs';
 
 export default async function Home() {
-  const user = await currentUser();
+  const user = await currentUser()
+  const today = new Date().toISOString().slice(0, 10)
   return (
     <div className='flex justify-center mt-14'>
       <div>
         <div>Welcome {user?.firstName}</div>
+        <div>Today is {today}</div>
       </div>
     </div>
   )
