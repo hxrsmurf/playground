0ec31606b928376e13220fc7b63878226c760fd4
Kevin <kevin@hxrsmurf.info>
2023-06-19 00:54:22 -0400

    Create upload_to_upstash

--- nextjs-journal/python/functions/upstash.py
+++ nextjs-journal/python/functions/upstash.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
 import redis
 import os
+import json
+
+load_dotenv()
 
 client = redis.Redis(
     host=os.getenv("UPSTASH_SERVER"),
@@ -8,6 +12,11 @@ client = redis.Redis(
     ssl=True,
 )
 
+
 def upload_to_upstash(list_content_all_paths):
     for path in list_content_all_paths:
-        print(path)
+        user = os.getenv("USER")
+        title = path["title"]
+        json_path = json.dumps(path)
+        client.hset(user, key=title, value=json_path, mapping=None, items=None)
+        print(title)
