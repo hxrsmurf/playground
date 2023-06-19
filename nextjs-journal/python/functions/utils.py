2d4b6d53e26e068b37fd5924f0f34d361e5ce87d
Kevin <kevin@hxrsmurf.info>
2023-06-19 00:21:35 -0400

    Add convert title to string timestamp

--- nextjs-journal/python/functions/utils.py
+++ nextjs-journal/python/functions/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 
 def read_json_file(file):
@@ -27,15 +28,41 @@ def get_path_files(path):
                 and not "Movies" in full_path
                 and not "Review" in full_path
             ):
-                list_of_full_paths.append(full_path)
+                list_of_full_paths.append({"full_path": full_path, "file": file})
     return list_of_full_paths
+
 
 def get_contents_all_paths(list_of_full_paths):
     list_contents = []
     for path in list_of_full_paths:
-        list_contents.append({
-            'path': path,
-            'contents': get_file_content(path)
-        })
-        break
+        full_path = path["full_path"]
+        file = path["file"]
+        contents = get_file_content(full_path)
+
+        list_contents.append(
+            {"full_path": full_path, "contents": contents, "file": file}
+        )
     return list_contents
+
+
+def convert_file_to_date(file, type):
+    # 12_15_2021
+    if type == "underscore":
+        return datetime.strptime(file, "%m_%d_%Y").strftime("%Y-%m-%d")
+
+
+def get_title_from_full_path(full_path):
+    underscore_split = full_path.split(" ")
+    try:
+        return convert_file_to_date(underscore_split[0], "underscore")
+    except Exception as e:
+        print('get_title_from_full_path:', e)
+
+
+def parse_contents_paths(contents_paths):
+    list_parsed_contents = []
+    for c in contents_paths:
+        title = get_title_from_full_path(c["file"])
+        c["title"] = title
+        list_parsed_contents.append(c)
+    return list_parsed_contents
