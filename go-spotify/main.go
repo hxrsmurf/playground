fa732017680dd5d3d6e983812b1f56d9347a8bac
Kevin <kevin@hxrsmurf.info>
2023-09-23 19:16:21 -0400

    simple user profile

--- go-spotify/main.go
+++ go-spotify/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -20,6 +21,10 @@ type TokenResponse struct {
 	ExpiresIn    string `json:"expires_in"`
 	RefreshToken string `json:"refresh_token"`
 	Scope        string `json:"scope"`
+}
+
+type User struct {
+	DisplayName string `json:"display_name"`
 }
 
 func main() {
@@ -95,6 +100,24 @@ func callback(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(bytes, &tokenResponse)
 	accessToken := tokenResponse.AccessToken
-	fmt.Fprint(w, accessToken)
-	fmt.Println(accessToken)
+	userResp := user(accessToken)
+	message := "Welcome " + userResp.DisplayName
+	fmt.Fprint(w, message)
+}
+
+func user(accessToken string) User {
+	url := "https://api.spotify.com/v1/me"
+	token := "Bearer " + accessToken
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Set("Authorization", token)
+
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	var user User
+	json.Unmarshal(body, &user)
+	return user
 }
