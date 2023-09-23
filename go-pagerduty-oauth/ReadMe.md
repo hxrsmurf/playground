8eba3ab502064cad3f9314f5a923867d0ac58c71
Kevin Murphy <kevin@hxrsmurf.info>
2023-09-23 14:39:20 -0400

    Create go-pagerduty-oauth (#58)
    * Add Read Me
    
    * Add .env.sample
    
    * Add go mod
    
    * add go sum
    
    * add main.go
    
    * Create go-pagerduty-oauth.yml
    
    * Go get and go build
    
    * Update go-pagerduty-oauth.yml
    
    * List directory
    
    * absolute path
    
    * Update go-pagerduty-oauth.yml
    
    * Add simple check for env
    
    * update workflow

---
+++ go-pagerduty-oauth/ReadMe.md
@@ -0,0 +1,9 @@
+# Read Me
+Super basic OAuth with GoLang. It creates a local HTTP server that you can use.
+
+1. Create OAuth App in PagerDuty with appropriate scopes and redirect uri
+2. Copy `.env.sample` to `.env`
+3. `go run main.go` or compile and run (`go build` and `./pd-oauth-go`)
+
+# Docs
+- https://developer.pagerduty.com/docs/a708c20fb57e5-obtaining-a-user-o-auth-token-via-pkce
---
+++ go-pagerduty-oauth/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+func code(w http.ResponseWriter, r *http.Request) {
+	queryValues := r.URL.Query()
+	code := queryValues.Get("code")
+	fmt.Fprint(w, code)
+	fmt.Println(code)
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	urlTemplate := "https://identity.pagerduty.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPE}&response_type=code&code_challenge_method=S256&code_challenge"
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Error loading .env file: %v", err)
+	}
+
+	// Load variables from .env
+	clientId := os.Getenv("clientId")
+	redirectURI := os.Getenv("redirectURI")
+	scope := os.Getenv("scope")
+
+	// Replace placeholders with actual values
+	url := strings.ReplaceAll(urlTemplate, "{CLIENT_ID}", clientId)
+	url = strings.ReplaceAll(url, "{REDIRECT_URI}", redirectURI)
+	url = strings.ReplaceAll(url, "{SCOPE}", scope)
+
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func checkEnv() int {
+	_, err := os.Stat("./.env")
+
+	if err != nil {
+		fmt.Println(".env does not exist")
+		return 0
+	} else {
+		return 1
+	}
+}
+func main() {
+	envFile := checkEnv()
+	if envFile == 0 {
+		return
+	}
+	fmt.Println("http://localhost:8000/auth")
+	http.HandleFunc("/auth", auth)
+	http.HandleFunc("/", code)
+	http.ListenAndServe(":8000", nil)
+}
