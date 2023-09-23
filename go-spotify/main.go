d6c23c11df5cf78eff4c01a1879c4fde9b41a8d9
Kevin <kevin@hxrsmurf.info>
2023-09-23 17:38:10 -0400

    Add hello world

--- go-spotify/main.go
+++ go-spotify/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -17,4 +19,11 @@ func main() {
 	clientId := os.Getenv("clientId")
 	clientSecret := os.Getenv("clientSecret")
 	fmt.Println(clientId, clientSecret)
+
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World!\n")
 }
