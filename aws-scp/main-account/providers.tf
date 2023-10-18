62bf733b6a246c844a49a7326a5215656361eb30
Kevin Murphy <kevin@hxrsmurf.info>
2023-10-18 21:18:11 +0000

    create sub-folder main account

---
+++ aws-scp/main-account/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.21.0"
+    }
+  }
+  cloud {
+    organization = "playground-repo"
+
+    workspaces {
+      name = "aws-scp"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
